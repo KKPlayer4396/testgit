d1d7699e7d2d725cd336b7459bbcdd6dd7abc10e
wskk <825575081@qq.com>
2023-11-13 20:42:42 +0800

    第一次提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ package org.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bbb222!");
+        System.out.println("第一次提交!");
     }
 
 }
