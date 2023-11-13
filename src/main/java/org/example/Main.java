cddbcfafc1e3c075b3e87a2667bbc9efe0e0b0bf
wskk <825575081@qq.com>
2023-11-13 20:07:12 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author wskk
+ * @description
+ * @Date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
